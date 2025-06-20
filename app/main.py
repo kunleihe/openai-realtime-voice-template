343d019c3495827dcf8bd732030840d32c06522d
Kunlei He <kunleih@uci.edu>
2025-06-19 21:34:03 -0700

    Initial commit

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.routes.health_check import health_check_router
 from app.routes.realtime import realtime_router
 
 app = FastAPI()
 
-# Registering routers
+# Registering routers first (before static files)
 app.include_router(health_check_router)
 app.include_router(realtime_router)
+
+# Mount static files from client directory
+app.mount("/", StaticFiles(directory="client", html=True), name="static")
